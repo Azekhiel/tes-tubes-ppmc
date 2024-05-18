c18e84227e0dcc6bd1237dfc6945b8fbf1e767f8
William <115348219+Azekhiel@users.noreply.github.com>
2024-05-18 16:21:03 +0700

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compile the program
-gcc -o main.c -lm
+gcc -o aco_program main.c src/ACO.c -I src -lm
 
 # Test case 1
-echo -e "kota_05.csv\nMakassar" | output > output1.txt
+echo -e "kota_05.csv\nMakassar" | ./aco_program > output1.txt
 
 # Test case 2
-echo -e "kota_16.csv\nJayapura" | output > output2.txt
+echo -e "kota_16.csv\nJayapura" | ./aco_program > output2.txt
