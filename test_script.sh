54912406d1fb45f6e721ea9491bfdfd48710a92f
William <115348219+Azekhiel@users.noreply.github.com>
2024-05-18 16:07:39 +0700

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -5,20 +5,6 @@ gcc -o aco_program main.c ACO.c -lm
 
 # Test case 1
 echo -e "kota_05.csv\nMakassar" | ./aco_program > output1.txt
-if grep -q "Jarak Tempuh:" output1.txt; then
-    echo "Test case 1 passed."
-else
-    echo "Test case 1 failed."
-    cat output1.txt
-    exit 1
-fi
 
 # Test case 2
 echo -e "kota_16.csv\nJayapura" | ./aco_program > output2.txt
-if grep -q "Jarak Tempuh:" output2.txt; then
-    echo "Test case 2 passed."
-else
-    echo "Test case 2 failed."
-    cat output2.txt
-    exit 1
-fi
